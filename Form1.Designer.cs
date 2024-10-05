2978f26f872ce36d65eed110c696b0276029f541
Miriam Irimia <irimiamiriam100@gmail.com>
2024-10-05 22:18:58 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Capracu3iezi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Ivory;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 281);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(455, 261);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Ivory;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(513, 281);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(455, 261);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Ivory;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(1000, 281);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(455, 261);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Ivory;
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(24, 567);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(455, 261);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Ivory;
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(513, 567);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(455, 261);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Ivory;
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox6.Location = new System.Drawing.Point(1000, 567);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(455, 261);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(265, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(980, 34);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Aseaza imaginile in ordinea cronologica intamplarii lor in Capra cu 3 iezi";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(158, 853);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(580, 57);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Verifica";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(24, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1447, 153);
+            this.panel1.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGreen;
+            this.button2.Font = new System.Drawing.Font("Modern No. 20", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(785, 853);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(580, 57);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1506, 931);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Capra cu 3 iezi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capracu3iezi
+{
+    public partial class Form1 : Form
+    {
+        List<Imagine> imagini = new List<Imagine>();
+        string[] files;
+        int x = 20, y = 0;
+        PictureBox pic;
+        int i = 0;
+        int corecte;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Initialisation();
+
+        }
+        private void Initialisation()
+        {
+            panel1.Controls.Clear();
+           
+            x = 20; y = 0;
+            pic = null;
+            i = 0;
+            corecte = 0;
+            foreach (Control c in this.Controls)
+            {
+                if (c is PictureBox)
+                {
+                    PictureBox pictureBox = (PictureBox)c;
+                    pictureBox.AllowDrop = true;
+                    pictureBox.DragEnter += pictureBox_DragEnter;
+                    pictureBox.DragDrop += pictureBox_DragDrop;
+                    pictureBox.Image = null;
+                }
+            }
+            string workingdirectory = Environment.CurrentDirectory;
+            string projectdirectory = Directory.GetParent(workingdirectory).Parent.FullName;
+            string path = Path.Combine(projectdirectory, "Resurse");
+            files = Directory.GetFiles(path);
+            Random random = new Random();
+            files = files.OrderBy(r => random.Next()).ToArray();
+            foreach (string file in files)
+            {
+                string[] line = file.Split('\\');
+                string nr = line[line.Length - 1].Split('.')[0];
+                Imagine imagine = new Imagine()
+                {
+                    image = Image.FromFile(file),
+                    initialloc = new Point(x, y),
+                    nr = Convert.ToInt32(nr)
+                };
+                imagini.Add(imagine);
+                x += 90 + 30;
+            }
+            foreach (Imagine imagine in imagini)
+            {
+                PictureBox pictureBox = new PictureBox()
+                {
+                    Image = imagine.image,
+                    Name = "raspuns" + i,
+                    Location = imagine.initialloc,
+                    BorderStyle = BorderStyle.FixedSingle,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Width = 90,
+                    Height = 70,
+                    Tag = imagine.nr
+                }; i++;
+                pictureBox.MouseDown += pictureBox_MouseDown;
+                panel1.Controls.Add(pictureBox);
+            }
+        }
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            var img = (sender as PictureBox).Image;
+            pic = sender as PictureBox;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            {
+                (sender as PictureBox).Image = null;
+            }
+        }
+        void pictureBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Bitmap))
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(var c in this.Controls)
+            {
+                if(c is PictureBox)
+                {
+                    PictureBox pb = (PictureBox)c;
+                    if (!pb.Name.Contains("raspuns")) 
+                    {
+                        string name= pb.Name;
+                        string n =Convert.ToString( name[name.Length-1]);
+                        int nr = Convert.ToInt32(n);
+                        if(nr == Convert.ToInt32(pb.Tag))
+                        {
+                            corecte++;
+                        }
+                    }
+                }
+            }
+            if(corecte == 6)
+            {
+                MessageBox.Show("Bravo!", "Ai asezat corect!");
+                Initialisation();
+
+            }
+            else
+            {
+                MessageBox.Show("Ups!", "Mai incearca o data!");
+                Initialisation();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Initialisation();
+        }
+
+        void pictureBox_DragDrop(object sender, DragEventArgs e)
+        {
+            var bmp = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+            if ((sender as PictureBox).Image == null)
+            {
+                (sender as PictureBox).Tag = pic.Tag;
+            }
+            else
+            {
+                Image image = (sender as PictureBox).Image;
+                int nr = Convert.ToInt32((sender as PictureBox).Tag);
+                (sender as PictureBox).Tag = pic.Tag;
+               
+                foreach (Control c in panel1.Controls) {
+                    if (c is PictureBox && c.Tag!=null) {
+                        if (c.Tag.ToString() == nr.ToString()) { PictureBox p =  (PictureBox)c;
+                            p.Image = image; }
+                    }
+                }
+                
+            }
+           (sender as PictureBox).Image = bmp;
+        }
+    }
+}
